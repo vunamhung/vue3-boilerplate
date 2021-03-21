08ca73368656bf419db1b07e7723328d8b0d0afa
Hung Vu <vunamhung@gmail.com>
2021-03-21 10:40:35 +0700

    use router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,3 @@
 <template>
-  <img class="inline-block" alt="Vue logo" src="./assets/images/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <router-view />
 </template>
-
-<script setup>
-import HelloWorld from './components/HelloWorld.vue';
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
-</script>
-
-<style lang="postcss">
-#app {
-  @apply text-center;
-}
-</style>
---
+++ src/router.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from './pages/Home.vue';
+
+const routes = [
+  {
+    path: '/',
+    component: Home,
+  },
+];
+
+const history = createWebHistory();
+
+const router = createRouter({ history, routes });
+export default router;
