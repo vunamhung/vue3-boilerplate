413d6e691e7c5a11968cad4d315cd7423910d6d6
Hung Vu <vunamhung@gmail.com>
2021-03-21 18:43:52 +0700

    vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA()],
   resolve: {
     alias: [
       {
